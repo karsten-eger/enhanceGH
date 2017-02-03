ce6b9cacdc234b47e03969b4d8979470d501e603
Karsten Eger <karsteneger@biker-boarder.de>
2017-02-03 12:58:22 +0100

    initial commit

---
+++ enhanceGithub.js
@@ -0,0 +1,36 @@
+var pushState = history.pushState;
+history.pushState = function () {
+    pushState.apply(history, arguments);
+    enhanceGitHub();
+};
+
+function enhanceGitHub() {
+// Add Whitespace Button to Pull Request File Change List
+if (window.location.href.indexOf('/files') !== -1) {
+    var prButtons = document.querySelector('.pr-review-tools');
+    var buttonDiv = document.createElement('div');
+    buttonDiv.className = 'diffbar-item';
+    var w1Button = document.createElement('a');
+    w1Button.href = '?w=1';
+    w1Button.className = 'btn btn-sm btn-primary';
+    w1Button.append(document.createTextNode('No Whitespace'));
+    buttonDiv.append(w1Button);
+    prButtons.append(buttonDiv);
+}
+
+// Add Whitespace Button to Pull Request File Change List
+if (window.location.href.indexOf('/pulls') !== -1) {
+    var subNav = document.querySelector('.subnav-links');
+    var buttonAll = document.createElement('a');
+    buttonAll.href = 'pulls';
+    buttonAll.className = 'js-selected-navigation-item subnav-item';
+    buttonAll.append(document.createTextNode('All'));
+    var buttonNew = document.createElement('a');
+    buttonNew.className = 'js-selected-navigation-item subnav-item';
+    buttonNew.href = 'pulls?utf8=✓&q=is%3Aopen is%3Apr -label%3A"request changes" -label%3A"WIP"';
+    buttonNew.append(document.createTextNode('Only New'));
+    subNav.prepend(buttonNew);
+    subNav.prepend(buttonAll);
+}
+}
+enhanceGitHub();
