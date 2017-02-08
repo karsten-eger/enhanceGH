39b9f59df2b44bc4815a0768a1ef825d48582244
= <github@mail.c3-net.de>
2017-02-09 00:03:20 +0100

    Major improvements
    Save/get filters to/from localstorage, extended Readme

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
+## Changes to Pull Request page
+With a lot of pull requests it's hard to identify which ones need your attention first. Sadly GitHub.com does not provide an option to save filters.
+This Tampermonkey/Greasemonkey scripts adds the option to save the current filter (in localStorage) and lists all of them in the filters dropdown.
 
+## Changes to "Files changed" page
+There's a neat feature hidden behind a mostly unknown parameter on the "files changed" page. When adding the parameter "w=1" to the query whitespace changes won't show up. To help with the ease of use a new button is added to provide this functionality instantly without tinkering with the URL.
 
-für die Verwendung im Grease-/Tampermonkey
+## How To
+To use the script just download Tampermonkey (Google Chrome) or Greasemonkey (Firefox) and add a new script. Then just copy-paste the following into the provided code editor:
 
 ```Javascript
 // ==UserScript==
 // @name        Enhanced Github
 // @namespace   Github
-// @description Github erweitern
+// @description enhance Steam Pull Request pages
 // @include     https://github.com/*
-// @require     https://raw.githubusercontent.com/CaesarIII/enhanceGH/master/enhanceGithub.js
+// @require     https://raw.githubusercontent.com/karsten-eger/enhanceGH/master/enhanceGithub.js
 // @version     1
 // @grant       none
 // ==/UserScript==
--- enhanceGithub.js
+++ enhanceGithub.js
@@ -3,38 +3,92 @@ history.pushState = function () {
     pushState.apply(history, arguments);
     enhanceGitHub();
 };
-
 window.addEventListener('pjax:end', function(){
     enhanceGitHub();
 });
-
-function enhanceGitHub() {
-// Add Whitespace Button to Pull Request File Change List
-if (window.location.href.indexOf('/files') !== -1 && !document.querySelector('.ehgh-buttons')) {
-    var prButtons = document.querySelector('.pr-review-tools');
-    var buttonDiv = document.createElement('div');
-    buttonDiv.className = 'diffbar-item ehgh-buttons';
-    var w1Button = document.createElement('a');
-    w1Button.href = '?w=1';
-    w1Button.className = 'btn btn-sm btn-primary';
-    w1Button.append(document.createTextNode('No Whitespace'));
-    buttonDiv.append(w1Button);
-    prButtons.append(buttonDiv);
-}
-
-// Add Filter Buttons to Pull Request List
-if (window.location.href.indexOf('/pulls') !== -1 && !document.querySelector('.ehgh-buttons')) {
-    var subNav = document.querySelector('.subnav-links');
-    var buttonAll = document.createElement('a');
-    buttonAll.href = 'pulls';
-    buttonAll.className = 'js-selected-navigation-item subnav-item ehgh-buttons';
-    buttonAll.append(document.createTextNode('All'));
-    var buttonNew = document.createElement('a');
-    buttonNew.className = 'js-selected-navigation-item subnav-item ehgh-buttons';
-    buttonNew.href = 'pulls?utf8=✓&q=is%3Aopen is%3Apr -label%3A"request changes" -label%3A"WIP"';
-    buttonNew.append(document.createTextNode('Only New'));
-    subNav.prepend(buttonNew);
-    subNav.prepend(buttonAll);
-}
-}
 enhanceGitHub();
+function enhanceGitHub() {
+    // Add Whitespace Button to Pull Request File Change List
+    if (window.location.href.indexOf('/files') !== -1 && !document.querySelector('.ehgh-buttons')) {
+        var prButtons = document.querySelector('.pr-review-tools');
+        var buttonDiv = document.createElement('div');
+        buttonDiv.className = 'diffbar-item ehgh-buttons';
+        var w1Button = document.createElement('a');
+        w1Button.href = '?w=1';
+        w1Button.className = 'btn btn-sm btn-primary';
+        w1Button.append(document.createTextNode('No Whitespace'));
+        buttonDiv.append(w1Button);
+        prButtons.append(buttonDiv);
+    }
+
+    // Add Filter Buttons to Pull Request List
+    if (window.location.href.indexOf('/pulls') !== -1 && !document.querySelector('.ehgh-buttons')) {
+        var filterContainer = document.querySelector('div.subnav div[role="search"]');
+        var filtersElement = filterContainer.querySelector('.select-menu-list');
+        var repoString = window.location.pathname;
+        var filterString = window.location.search;
+        var elementTemplate = filtersElement.querySelector('.select-menu-item.js-navigation-item:first-child').cloneNode(1);
+        elementTemplate.className += ' ehGHfilter';
+        var filterHelp = filtersElement.querySelector('.select-menu-item.js-navigation-item:last-child');
+
+        var filters = {};
+        ehGHinitFilters();
+
+        function ehGHaddFilter() {
+            var filterName = prompt('Name your new filter!', 'new filter');
+            if (!filterName) return;
+
+            filterObject = {name : filterName, search : filterString};
+            if (!filters[repoString]) {
+                filters[repoString] = [];
+            }
+            filters[repoString].push(filterObject);
+            localStorage.setItem('filters', JSON.stringify(filters));
+            ehGHinitFilters();
+        }
+
+        function ehGHremoveFilter(name) {
+            handler = JSON.parse(localStorage.getItem('filters'));
+            if (handler[repoString]) {
+                Object.keys(handler[repoString]).map(function(objectKey, index) {
+                    if (handler[repoString][objectKey]['name'] == name) {
+                        handler[repoString].splice(objectKey, 1);
+                    }
+                });
+            }
+            localStorage.setItem('filters', JSON.stringify(handler));
+            ehGHinitFilters();
+        }
+
+        function ehGHinitFilters() {
+            // Delete existing nodes
+            var ownFilters = filtersElement.querySelectorAll('.ehGHfilter');
+            Object.keys(ownFilters).map(function(objectKey, index) {
+                ownFilters[objectKey].remove();
+            });
+
+            // Add new nodes for all saved filters
+            filters = JSON.parse(localStorage.getItem('filters')) || {};
+            if (filters && filters[repoString]) {
+                Object.keys(filters[repoString]).map(function(objectKey, index) {
+                    filter = elementTemplate.cloneNode(1);
+                    filter.querySelector('div').innerHTML = filters[repoString][objectKey]['name'];
+                    filter.href = filters[repoString][objectKey]['search'];
+                    var deleteSpan = document.createElement('span');
+                    deleteSpan.className = 'select-menu-item-icon';
+                    deleteSpan.style = 'width: 18px;text-align: center;margin-left: -25px;';
+                    deleteSpan.append(document.createTextNode('x'));
+                    deleteSpan.addEventListener('click', function(e){e.preventDefault();ehGHremoveFilter(filters[repoString][objectKey]['name'])});
+                    filter.prepend(deleteSpan);
+                    filtersElement.insertBefore(filter, filterHelp);
+                });
+            }
+            // add "Add Filter" Button
+            addFilter = elementTemplate.cloneNode(1);
+            addFilter.querySelector('div').innerHTML = 'Save current filter';
+            addFilter.href = '#';
+            addFilter.addEventListener('click', function(e){e.preventDefault();ehGHaddFilter()});
+            filtersElement.insertBefore(addFilter, filterHelp);
+        }
+    }
+}
